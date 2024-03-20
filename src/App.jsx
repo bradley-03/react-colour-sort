90de6d5333b4b356b1732060fabaa9683aab71ac
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2024-03-20 13:34:01 +0000

    feat: basic image uploading

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ImageUploading from 'react-images-uploading'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [images, setImages] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const onChange = (imageList, addUpdateIndex) => {
+		// data for submit
+		console.log(imageList, addUpdateIndex);
+		setImages(imageList);
+	};
+
+	return (
+		<>
+			<ImageUploading
+				multiple
+				value={images}
+				onChange={onChange}
+				dataURLKey={'data_url'}
+			>
+				{({
+					imageList,
+					onImageUpload,
+					onImageRemoveAll,
+					onImageUpdate,
+					onImageRemove,
+					isDragging,
+					dragProps
+				}) => (
+					// write your building UI
+					<div className="upload__image-wrapper">
+						<button
+							style={isDragging ? { color: "red" } : null}
+							onClick={onImageUpload}
+							{...dragProps}
+						>
+							Click or Drop here
+						</button>
+						&nbsp;
+						<button onClick={onImageRemoveAll}>Remove all images</button>
+						{imageList.map((image, index) => (
+							<div key={index} className="image-item">
+								<img src={image.data_url} alt="" width="100" />
+								<div className="image-item__btn-wrapper">
+									<button onClick={() => onImageUpdate(index)}>Update</button>
+									<button onClick={() => onImageRemove(index)}>Remove</button>
+								</div>
+							</div>
+						))}
+					</div>
+				)}
+			</ImageUploading>
+		</>
+	)
 }
 
 export default App
