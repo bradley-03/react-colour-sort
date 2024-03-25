99148770466bba02617efaeadddc4c97c46ba9c6
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2024-03-25 14:27:55 +0000

    style: moved buttons into overlay

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,11 +1,15 @@
-export default function Image ({image, index, onImageUpdate, onImageRemove}) {
+import './Image.css'
+
+export default function Image({ image, index, onImageUpdate, onImageRemove }) {
     return (
-        <div className="image-item">
-        <img src={image.data_url} alt="" style={{ height: '150px', width: '150px', objectFit: 'cover' }} />
-        <div className="image-item__btn-wrapper">
-            <button onClick={() => onImageUpdate(index)}>Update</button>
-            <button onClick={() => onImageRemove(index)}>Remove</button>
+        <div className="Image">
+            <img src={image.data_url} alt="" />
+            <div className="overlay">
+                <div className='buttons'>
+                    <button className='overlaybtn' onClick={() => onImageUpdate(index)}>Update</button>
+                    <button className='overlaybtn' onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+            </div>
         </div>
-    </div>
     )
 }
