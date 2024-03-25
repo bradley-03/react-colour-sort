228c3f383a6ff1b5a912890596673ff4fc7189d7
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2024-03-25 13:03:51 +0000

    feat: added loader when adding images

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 import ImageUploading from 'react-images-uploading'
-import {prominent} from 'color.js'
+import { prominent } from 'color.js'
+import { dotWave } from 'ldrs'
 
 function App() {
 	const [images, setImages] = useState([])
+	const [loading, setLoading] = useState(false)
+
+	dotWave.register()
 
 	const onChange = async (imageList, addUpdateIndex) => {
 		// data for submit
 		// console.log(imageList, addUpdateIndex)
 
+		setLoading(true)
 		for (let [index, image] of imageList.entries()) {
 			try {
-				const prominentColor = await prominent(image["data_url"], {amount: 1})
-				imageList[index]["prominent_color"] = prominentColor
+				if (!imageList[index]["prominent_color"]) {
+					const prominentColor = await prominent(image["data_url"], { amount: 1 })
+					imageList[index]["prominent_color"] = prominentColor
+				}
 			} catch (e) {
 				console.log(e)
 			}
 		}
+		setLoading(false)
 
 		setImages(imageList);
 	};
@@ -50,18 +58,20 @@ function App() {
 						</button>
 						&nbsp;
 						<button onClick={onImageRemoveAll}>Remove all images</button>
-						<div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '15px', flexWrap: 'wrap'}}>
-						{imageList.map((image, index) => (
-							<div key={index} className="image-item">
-								<img src={image.data_url} alt="" style={{height: '150px', width: '150px', objectFit: 'cover'}} />
-								<div className="image-item__btn-wrapper">
-									<button onClick={() => onImageUpdate(index)}>Update</button>
-									<button onClick={() => onImageRemove(index)}>Remove</button>
+						<br></br>
+						{loading && <l-dot-wave size="50" speed="1" color="white" style={{marginTop: '10px'}} />}
+						<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '15px', flexWrap: 'wrap' }}>
+							{imageList.map((image, index) => (
+								<div key={index} className="image-item">
+									<img src={image.data_url} alt="" style={{ height: '150px', width: '150px', objectFit: 'cover' }} />
+									<div className="image-item__btn-wrapper">
+										<button onClick={() => onImageUpdate(index)}>Update</button>
+										<button onClick={() => onImageRemove(index)}>Remove</button>
+									</div>
 								</div>
-							</div>
-						))}
+							))}
 						</div>
-						
+
 					</div>
 				)}
 			</ImageUploading>
